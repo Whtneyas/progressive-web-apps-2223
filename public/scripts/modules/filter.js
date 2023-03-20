d897500128aad999cb868e72427c237a7036c255
Whitneyas <whitneyserwaa@gmail.com>
2023-03-20 14:09:40 +0100

    modules added

---
+++ public/scripts/modules/filter.js
@@ -0,0 +1,26 @@
+// import fetchBibleQuotes from "./fetchBibleQuotes.js";
+// console.log(fetchBibleQuotes());
+
+// export const chapterFilter = document.getElementById("chapter");
+// export const verseFilter = document.getElementById("verse");
+
+
+// //Filter the chapter and verses
+// export const filterBibleQuotes = () => {
+
+//   const selectedChapter = chapterFilter.value;
+//   const selectedVerse = verseFilter.value;
+
+//   const filteredQuotes = fetchBibleQuotes.filter((quote) => {
+//     return quote.chapter === selectedChapter && quote.verse === selectedVerse;
+//   });
+
+//   currentIndex = 0;
+//   displayBibleQuote(filteredQuotes[currentIndex]);
+// };
+
+// chapterFilter.addEventListener("change", filterBibleQuotes);
+// verseFilter.addEventListener("change", filterBibleQuotes);
+
+
+// export default filterBibleQuotes
