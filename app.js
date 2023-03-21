808d75aee00914c5400374ddefa698d17798019a
Whitneyas <whitneyserwaa@gmail.com>
2023-03-21 21:48:39 +0100

    app.js adjusted

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { engine } from 'express-handlebars';
+import express from 'express'
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
+//Sets the app to use the handlebars engine
+app.set('view engine', 'ejs');
 
-app.engine('handlebars', engine());
-app.engine(
-	'hbs',
-	engine({
-		layoutsDir: `${path.join(__dirname)}/views`,
-		partialsDir: `${path.join(__dirname)}/views/partials`,
-		defaultLayout: 'main',
-		extname: '.hbs'
-	})
-);
-app.set('view engine', 'hbs');
-app.set('views', './views');
+app.set('views', 'views')
 
-app.get('/', (req, res) => {
-	res.render('home', { title: 'Home' });
-});
+app.use(express.static('public'));
+
+// //Get route file 
+// export const getRouter = express.Router();
+
+// // Tell express to use posts.js file for the routes 
+// app.use('/' , getRouter)
+
+//creating the homepage
+app.get('/' , (req, res) =>{
+	res.render('index')
+
+})
+
+// create the aboutpage
+
+app.get('/about', (req, res) =>{
+	res.render('aboutpage')
+
+})
+
+
+
+
+
 
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
