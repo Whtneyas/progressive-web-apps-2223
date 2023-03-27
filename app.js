2cbdfbc3788f62dec23c21eeafedc0d47f1e1ac5
Whitneyas <whitneyserwaa@gmail.com>
2023-03-27 16:21:43 +0200

    spent 10hrs fixing detailpage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
-
-
 const app = express();
+const request = require('request');
+
 const port = process.env.PORT || 3000;
 
 //Sets the app to use the handlebars engine
@@ -11,30 +11,78 @@ app.set('views', 'views')
 
 app.use(express.static('public'));
 
-// //Get route file 
-const getRouter =require ('./routes/posts');
+// Home page
+app.get('/', (req, res) => {
+	const apiUrl = 'https://bible-api.com/romans%201';
 
-// Tell express to use posts.js file for the routes 
-app.use('/', getRouter)
-
-// //creating the homepage
-// app.get('/' , (req, res) =>{
-// 	res.render('index')
-
-// })
-
-// // create the aboutpage
-
-// app.get('/about', (req, res) =>{
-// 	res.render('aboutpage')
-
-// })
-
-
+	request(apiUrl, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			const bibleData = JSON.parse(body);
+			const verses = bibleData.verses;
+			console.log(verses)
+			res.render('home', {
+				title: 'home',
+				verses: verses
+			});
+		} else {
+			console.log(error);
+			res.render('error');
+		}
+	});
+});
 
 
+
+//Detail page 
+app.get('/verse/:id', (req, res) => {
+	const apiUrl = `https://bible-api.com/${req.params.id}`;
+	request(apiUrl, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			const bibleData = JSON.parse(body);
+			const verse = bibleData.verses[0];
+			console.log(verse);
+			res.render('verse', {
+				title: verse.book_name,
+				verse: verse
+			});
+		} else {
+			console.log(error);
+			res.render('error');
+		}
+	});
+});
+
+
+//All verses 
+app.get('/verses', (req, res) => {
+	const apiUrl = 'https://bible-api.com/romans%201';
+
+	request(apiUrl, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			const bibleData = JSON.parse(body);
+			const verses = bibleData.verses;
+			console.log(verses)
+			res.render('verses', {
+				title: 'Verses',
+				verses: verses
+			});
+		} else {
+			console.log(error);
+			res.render('error');
+		}
+	});
+});
+
+
+
+//about page
+app.get('/about', (req, res) => {
+	res.render('aboutpage', {
+		title: 'Home'
+	})
+})
 
 
 app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at  http://localhost:${port}`);
 });
