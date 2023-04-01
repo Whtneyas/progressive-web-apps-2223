cd3eca5fc93317bf85e65d9cbf1aa421c7efcc28
Whitneyas <whitneyserwaa@gmail.com>
2023-04-01 19:14:50 +0200

    editted

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/offline',
+    '/css/styles.css',
+    '/css/overview.css',
+    'https://fonts.googleapis.com/css2?family=Athiti:wght@300;400&family=Tilt+Prism&display=swap'
+]
+
+//install service worker 
+self.addEventListener('install', evt => {
+    self.skipWaiting();
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+    console/log(assets)
+});
+//activate service worker
+self.addEventListener('activate', evt => {
+    // console.log('service worker has been activated');
+});
+
+//fetch events
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event', evt)
+
+});
