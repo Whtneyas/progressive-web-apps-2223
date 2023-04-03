7267259f1a5f7c174e942dcd81da348067248405
Whitneyas <whitneyserwaa@gmail.com>
2023-04-03 11:42:46 +0200

    node aap.js added to start

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,11 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
+const dynamicCache = 'site-dynamic'
 const assets = [
     '/',
     '/offline',
     '/css/styles.css',
-    'https://fonts.googleapis.com/css2?family=Athiti:wght@300;400&family=Tilt+Prism&display=swap'
+    'https://fonts.googleapis.com/css2?family=Athiti:wght@300;400&family=Tilt+Prism&display=swap',
+    '(https://fonts.gstatic.com/s/athiti/v12/pe0sMISdLIZIv1wAoDBCEfe_Kdxic2wp.woff2'
 ]
 
 //install service worker 
@@ -15,16 +17,41 @@ self.addEventListener('install', evt => {
             cache.addAll(assets);
         })
     );
-   
+
 });
+//dynamic caching
+
 
 //activate service worker
 self.addEventListener('activate', evt => {
     // console.log('service worker has been activated');
+
+    evt.waitUnil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCache)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
 });
 
 //fetch events
 self.addEventListener('fetch', evt => {
     // console.log('fetch event', evt)
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone())
+                    return fetchRes;
+                })
+            });
+        }).catch(() => caches.match('/offline'))
+
+    );
+
 
 });
+
